580a95387b57e6823353bde8ec3ac69814c26af2
brunojppb <brunojppb@users.noreply.github.com>
2019-10-26 19:46:06 +0000

    Github Actions new build - Sat Oct 26 19:46:06 UTC 2019

---
+++ precache-manifest.384de5bfb6bf955aea875d390d2c605c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9532ef7eba6042a2c295569dc25879bf",
+    "url": "/d-and-d-web/index.html"
+  },
+  {
+    "revision": "929be976505723a41363",
+    "url": "/d-and-d-web/static/css/main.2eb2cea6.chunk.css"
+  },
+  {
+    "revision": "d55d1ea3a3361955d8bb",
+    "url": "/d-and-d-web/static/js/2.60a853af.chunk.js"
+  },
+  {
+    "revision": "929be976505723a41363",
+    "url": "/d-and-d-web/static/js/main.2dca7b31.chunk.js"
+  },
+  {
+    "revision": "1ddf6abf4264c0352567",
+    "url": "/d-and-d-web/static/js/runtime-main.dff5de94.js"
+  },
+  {
+    "revision": "33d2b59fd559db4b80c1fcd863829ef8",
+    "url": "/d-and-d-web/static/media/antonio.33d2b59f.jpg"
+  },
+  {
+    "revision": "776140e412c3a046aa6b7396fa0dcd6e",
+    "url": "/d-and-d-web/static/media/bia.776140e4.jpg"
+  },
+  {
+    "revision": "e43b7e59393f7e73f45f373a4014c28c",
+    "url": "/d-and-d-web/static/media/goblin.e43b7e59.jpg"
+  },
+  {
+    "revision": "81572d03abdee4b915561052a61f7e07",
+    "url": "/d-and-d-web/static/media/lampiao_jr.81572d03.jpg"
+  },
+  {
+    "revision": "152c63cd351992cf78cbf7bde9506d33",
+    "url": "/d-and-d-web/static/media/lila.152c63cd.jpg"
+  },
+  {
+    "revision": "480341fab0d18c7e014858d6b5b6317b",
+    "url": "/d-and-d-web/static/media/rha.480341fa.jpg"
+  },
+  {
+    "revision": "827305cd6c106e39d51c1f72bdf5aeb0",
+    "url": "/d-and-d-web/static/media/rodo.827305cd.jpg"
+  },
+  {
+    "revision": "97c0a06777d7f7b3b4f95f79443ddd80",
+    "url": "/d-and-d-web/static/media/thief.97c0a067.jpg"
+  },
+  {
+    "revision": "aac9f645ac85e1d67595fbf41b78d84b",
+    "url": "/d-and-d-web/static/media/wolf.aac9f645.jpg"
+  }
+]);
